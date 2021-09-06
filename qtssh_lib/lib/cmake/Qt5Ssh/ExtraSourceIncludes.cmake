bcedb2ead31f979a4c3fb2942c4e9de9e810eb29
Infotronica <65802795+Infotronica@users.noreply.github.com>
2021-09-05 23:34:52 -0500

    Add files via upload

---
+++ qtssh_lib/lib/cmake/Qt5Ssh/ExtraSourceIncludes.cmake
@@ -0,0 +1,7 @@
+
+list(APPEND _Qt5Ssh_OWN_INCLUDE_DIRS
+    "C:/Users/bahro/OneDrive/Desktop/lib/Lib/QtSsh/include" "C:/Users/bahro/OneDrive/Desktop/lib/Lib/QtSsh/include/QtSsh"
+)
+set(Qt5Ssh_PRIVATE_INCLUDE_DIRS
+    "C:/Users/bahro/OneDrive/Desktop/lib/Lib/QtSsh/include/QtSsh/4.3.1" "C:/Users/bahro/OneDrive/Desktop/lib/Lib/QtSsh/include/QtSsh/4.3.1/QtSsh"
+)
